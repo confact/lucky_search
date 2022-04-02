4ab12f1044a45c3bb86ecd9a97b118f3b53b5f28
Håkan <hakan@dun.se>
2022-04-03 01:20:37 +0200

    try to fix db in test

--- spec/setup/setup_database.cr
+++ spec/setup/setup_database.cr
@@ -1,2 +1,41 @@
 Db::Create.new(quiet: true).call
 Db::Migrate.new(quiet: true).call
+
+
+database_name = "lucky_search_#{LuckyEnv.environment}"
+
+AppDatabase.configure do |settings|
+  if LuckyEnv.test?
+    settings.credentials = Avram::Credentials.parse?(ENV["DATABASE_URL"]?) || Avram::Credentials.new(
+      database: database_name,
+      hostname: ENV["DB_HOST"]? || "postgres",
+      port: ENV["DB_PORT"]?.try(&.to_i) || 5432,
+      # Some common usernames are "postgres", "root", or your system username (run 'whoami')
+      username: ENV["DB_USERNAME"]? || "",
+      # Some Postgres installations require no password. Use "" if that is the case.
+      password: ENV["DB_PASSWORD"]? || ""
+    )
+  else
+    settings.credentials = Avram::Credentials.parse?(ENV["DATABASE_URL"]?) || Avram::Credentials.new(
+      database: database_name,
+      hostname: ENV["DB_HOST"]? || "localhost",
+      port: ENV["DB_PORT"]?.try(&.to_i) || 5432,
+      # Some common usernames are "postgres", "root", or your system username (run 'whoami')
+      username: ENV["DB_USERNAME"]? || "postgres",
+      # Some Postgres installations require no password. Use "" if that is the case.
+      password: ENV["DB_PASSWORD"]? || "postgres"
+    )
+  end
+end
+
+Avram.configure do |settings|
+  settings.database_to_migrate = AppDatabase
+
+  # In production, allow lazy loading (N+1).
+  # In development and test, raise an error if you forget to preload associations
+  settings.lazy_load_enabled = LuckyEnv.production?
+
+  # Always parse `Time` values with these specific formats.
+  # Used for both database values, and datetime input fields.
+  # settings.time_formats << "%F"
+end
