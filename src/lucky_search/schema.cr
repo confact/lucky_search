6330aeb15fa01704f06a6bb62421f9971d2b5f6c
Håkan <hakan@dun.se>
2022-04-02 20:32:02 +0200

    format

--- src/lucky_search/schema.cr
+++ src/lucky_search/schema.cr
@@ -5,16 +5,15 @@ module LuckySearch
     def self.generate_schema(search_data)
       mappings = generate_field_type(search_data)
 
-
       {
         "settings" => {
           "number_of_shards"   => 1,
           "number_of_replicas" => 1,
-          "index" => {
-            "max_ngram_diff" => 49,
-            "max_shingle_diff" => 4
+          "index"              => {
+            "max_ngram_diff"   => 49,
+            "max_shingle_diff" => 4,
           },
-          "analysis"           => analysis,
+          "analysis" => analysis,
         },
         "mappings" => {
           "properties" => mappings,
